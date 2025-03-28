edd8e2c7c4d9561b97e5c8cf19931e17afde797d
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-28 14:58:01 -0600

    html(login) updated and necessary id cashed for logic use

---
+++ app.js
@@ -0,0 +1,4 @@
+// cash the registration, login and error display to use for logic
+const registerForm = document.getElementById("registration");
+const logForm = document.getElementById("login");
+const showErr = document.getElementById("errorDisplay");
