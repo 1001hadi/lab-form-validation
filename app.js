8873942d9e097dc7c8c3d42720205eb0699b170a
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-28 15:07:51 -0600

    error display helper function added

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 const registerForm = document.getElementById("registration");
 const logForm = document.getElementById("login");
 const showErr = document.getElementById("errorDisplay");
+
+// create helper functions to displaying errors, success and clear data
+
+// display Error helper
+function displayErr(errMsg) {
+  showErr.textContent = errMsg;
+  showErr.style.color = "red";
+}
